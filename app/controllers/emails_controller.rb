c946eec2fe3de960fce69a4c002c9536895aea91
Sabatol <u.sacrez@gmail.com>
2020-11-19 18:50:23 +0100

    update 2.4

---
+++ app/controllers/emails_controller.rb
@@ -0,0 +1,58 @@
+class EmailsController < ApplicationController
+
+require 'faker'
+
+  def index
+    @emails = Email.all
+  end
+
+  def show
+    @email = Email.find(params[:id])
+    @email.update(object: @email.object, body: @email.body, read: true)
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js { }
+    end
+  end
+
+  def new
+  end
+
+  def create
+    @email = Email.new(object: Faker::Lorem.word, body: Faker::Lorem.sentence)
+    if @email.save
+      respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js { }
+      end
+    else
+      alert("Essaye encore !")
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @email = Email.find(params[:id])
+    if @email.read
+      @email.update(object: @email.object, body: @email.body, read: false)
+    else
+      @email.update(object: @email.object, body: @email.body, read: true)
+    end
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js { }
+    end
+  end
+
+  def destroy
+    @email = Email.find(params[:id])
+    @email.destroy
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js { }
+    end
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,21 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Category.delete_all
+Email.delete_all
+
 require 'faker'
+
 3.times do
   my_category = Category.create(title: Faker::Book.genre)
-  3.times do
+  4.times do
     my_task = Task.new(title: Faker::Book.title,
                       deadline: Faker::Date.forward(23),
                       image: Faker::Avatar.image)
     my_task.category = my_category
     my_task.save
   end
+end
+
+10.times do
+  emails = Email.create(object: Faker::Esport.event,body: Faker::Book.title)
 end
